b8b524446e2cdbd728fb451b4de5202aaa7bf3c3
Damar2121 <damar.vivoy51@gmail.com>
2024-05-13 18:14:09 +0700

    default library

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -20,11 +20,34 @@ public:
 	Mahasiswa(string iNama) {
 		nama = iNama;
 	}
-	Mahasiswa(int iNim, string iNama);
-	void cetak();
+	Mahasiswa(int iNim, string iNama)
+	{
+		nim = iNim;
+		nama = iNama;
+	}
+	
+	void cetak() 
+	{
+		cout << "Nim  = " << nim << endl;
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+	}
 };
 
-Mahasiswa::Mahasiswa(int iNim)
+int main()
 {
-	nim = iNim;
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(20);
+	Mahasiswa mhs3("Bung Hatta");
+	Mahasiswa mhs4(20,"Bill Gates");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
 }
+
+
+
+
