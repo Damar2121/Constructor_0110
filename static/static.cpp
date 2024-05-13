71211e4d068a4e8731ec2837f4c6634ce246622f
Damar2121 <damar.vivoy51@gmail.com>
2024-05-13 10:30:13 +0700

    membuat class mahasiswa

--- static/static.cpp
+++ static/static.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa
+{
+	static int nim;
+	int id;
+	string nama;
+
+	void setId();
+	void printAll();
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setId();
+	}
+};
